1ead279673bd26a6b04aa73f54e1a751a6d81313
Shashant Shekhar <shashantshekhar10@gmail.com>
2023-09-12 01:48:14 +0530

    first commit

---
+++ src/components/WorkCard/WorkCardData.js
@@ -0,0 +1,77 @@
+import project1 from '../../assets/project1.png';
+import project2 from '../../assets/project2.png';
+import project3 from '../../assets/project3.png';
+import project4 from '../../assets/project4.png';
+import project5 from '../../assets/project5.png';
+import project6 from '../../assets/project6.png';
+import project7 from '../../assets/project7.png';
+import project8 from '../../assets/project8.png';
+// import project9 from '../../assets/project19.jpg';
+
+const WorkCardData = [
+    {
+        imgsrc: project1,
+        title: "Foodie-Hub",
+        text: "designed Foodie Hub, a React.js web application enabling users to search for dishes and access detailed information, including images, ingredients, and cooking instructions via external APIs.",
+        view: "https://foodie-hubb.netlify.app/",
+        source: "https://github.com/shashant654/Foodie-hub"
+    },
+    {
+        imgsrc: project2,
+        title: "Youtube-Clone",
+        text: "designed a YouTube clone using React, showcasing my expertise in modern front-end web development. Implemented key features such as video playback, user authentication, live Chat, and Comments with replies.",
+        view: "https://my-youtubee-clone.netlify.app/",
+        source: "https://github.com/shashant654/youtube_clone"
+    },
+    {
+        imgsrc: project3,
+        title: "Weather-Application",
+        text: "Developed a weather application using React, showcasing strong front-end development skills. Implemented real-time weather data retrieval through APIs, providing users with accurate forecasts.",
+        view: "https://weather-appllicationn.netlify.app/",
+        source: "https://github.com/shashant654/weather_application_using-react"
+    },
+    {
+        imgsrc: project4,
+        title: "TODO-LIST",
+        text: "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        view: "https://todo-listt-using-react.netlify.app/",
+        source: "https://github.com/shashant654/Todo_App_using-react"
+    },
+    {
+        imgsrc: project5,
+        title: "Quiz Application",
+        text: "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        view: "https://quiz-app-component.netlify.app/",
+        source: "https://github.com/shashant654/Quiz_app-using-react"
+    },
+    {
+        imgsrc: project6,
+        title: "Crypto-Converter",
+        text: " developed a dynamic cryptocurrency converter website using React, enabling users to seamlessly convert between various cryptocurrencies and fiat currencies. Leveraged API integrations to fetch real-time market data, ensuring accurate and up-to-date conversion rates.",
+        view: "https://crypto-converterr.netlify.app/",
+        source: "https://github.com/shashant654/Crypto-Converter_using_react"
+    },
+    {
+        imgsrc: project7,
+        title: "Pagination-Component",
+        text: "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It was popularised in the 1960s with the release of Letraset sheets It was popularised in the 1960s with the release of Letraset sheets It was popularised in the 1960s with the release of Letraset sheets",
+        view: "https://pagination-feature.netlify.app/",
+        source: "https://github.com/shashant654/Pagination-using-react"
+    },
+    {
+        imgsrc: project8,
+        title: "Infinite-Scrolling-Component",
+        text: "It was popularised in the 1960s with the release of Letraset sheets It was popularised in the 1960s with the release of Letraset sheets It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        view: "https://infinite-scrolling-feature.netlify.app/",
+        source: "https://github.com/shashant654/Infinite-Scrolling_using-react"
+    },
+    // {
+    //     imgsrc: project9,
+    //     title: "Trending movie website",
+    //     text: "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more It was popularised in the 1960s with the release of Letraset sheets It was popularised in the 1960s with the release of Letraset sheets recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    //     view: "https://glistening-mochi-5b7573.netlify.app/",
+    //     source: "https://github.com/kumarsantosh3914/DOM_Project/tree/master/furniture-addCard"
+    // },
+]
+
+export default WorkCardData;
